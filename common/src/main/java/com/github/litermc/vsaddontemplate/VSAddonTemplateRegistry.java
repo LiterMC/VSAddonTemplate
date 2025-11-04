d494854f923ff206293e9597ad052eaf947a96f9
zyxkad <zyxkad@gmail.com>
2025-11-04 14:44:43 -0700

    update to create 6

--- common/src/main/java/com/github/litermc/vsaddontemplate/VSAddonTemplateRegistry.java
+++ common/src/main/java/com/github/litermc/vsaddontemplate/VSAddonTemplateRegistry.java
@@ -27,6 +27,7 @@ import net.minecraft.world.level.material.PushReaction;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.BiConsumer;
 import java.util.function.BiFunction;
 
 public final class VSAddonTemplateRegistry {
