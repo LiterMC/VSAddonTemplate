e51842164f79c7cd911ffe788d14698cf6c5bc7e
zyxkad <zyxkad@gmail.com>
2026-01-15 21:34:52 -0700

    add attachment register example

--- fabric/src/main/java/com/github/litermc/vsaddontemplate/ModEntry.java
+++ fabric/src/main/java/com/github/litermc/vsaddontemplate/ModEntry.java
@@ -41,5 +41,7 @@ public class ModEntry implements ModInitializer {
 
 		ServerTickEvents.START_SERVER_TICK.register(VSAddonTemplateListeners::preServerTick);
 		ServerTickEvents.END_SERVER_TICK.register(VSAddonTemplateListeners::postServerTick);
+
+		VSAddonTemplateListeners.onModSetup();
 	}
 }
